f8494ba263d62b3ab35deaa0bee9eb109612741d
Giada-Ripamonti <ripamontigiada@gmail.com>
2022-05-10 17:51:13 +0200

    last-commit

--- script/script.js
+++ script/script.js
@@ -1,1 +1,23 @@
-window.alert('Welcome =)')
+window.alert('Ciao =) Vediamo se devi fare la spesa')
+
+var product = prompt ('Cosa ti serve?')
+var need = prompt ('Quanto te ne serve?')
+var have = prompt ('Quanto ne hai?')
+
+document.getElementById('product').innerHTML = product
+document.getElementById('need').innerHTML = need + (' ') + product;
+document.getElementById('have').innerHTML = have + (' ') + product;
+
+let num1 = parseInt(need)
+let num2 = parseInt(have)
+
+let result = num2 - num1;
+console.log(result)
+
+if(result >= num1) {
+    document.getElementById('grocery').innerHTML = ('Non ti manca nulla!')
+}
+
+if(result < num1) {
+    document.getElementById('grocery').innerHTML = ('Ti servono ancora: ') + -(result) + (' ') + product;
+}
